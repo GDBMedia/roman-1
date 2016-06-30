2fd0584311dba201fe28b998e09849dda1ad00d3
Garrett Biernat <garrett@gdbmedia.net>
2016-06-30 13:22:25 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -46,58 +46,6 @@ function romannumeral (array1){
 	var result = array1.join("");
 	return result;
 }
-// function romannumeral (array){
-// 	var arrayOfNumerals = ["I", "V", "X", "L", "C", "D", "M"];
-// 	// debugger;
-// 	var array1 = array.reverse();
-// 	var arrayofRoman = [];
-// 	var x = 0;
-// 	for(var i = 0; i < array1.length; i++){
-// 		if(array1[i] === "0"){
-// 			array1[i] = "";
-// 		}
-// 		if(array1[i] === "1"){
-// 			array1[i] = arrayOfNumerals[x];
-// 		}	
-// 		if(array1[i] === "2"){
-// 			array1[i] = arrayOfNumerals[x] + arrayOfNumerals[x] ;
-			
-// 		}
-// 		if(array1[i] === "3"){
-// 			array1[i] = arrayOfNumerals[x] + arrayOfNumerals[x] + arrayOfNumerals[x];
-			
-// 		}
-
-// 		if(array1[i] === "4"){
-// 			array1[i] =arrayOfNumerals[x] + arrayOfNumerals[x+1] ;
-			
-// 		}
-// 		if(array1[i] === "5"){
-// 			array1[i] = arrayOfNumerals[x+1];
-			
-// 		}
-// 		if(array1[i] === "6"){
-// 			array1[i] = arrayOfNumerals[x+1] + arrayOfNumerals[x] ;
-			
-// 		}
-// 		if(array1[i] === "7"){
-// 			array1[i] = arrayOfNumerals[x+1]+ arrayOfNumerals[x]+ arrayOfNumerals[x] ;
-			
-// 		}
-// 		if(array1[i] === "8"){
-// 			array1[i] = arrayOfNumerals[x+1]+ arrayOfNumerals[x]+ arrayOfNumerals[x]+ arrayOfNumerals[x] ;
-			
-// 		}
-// 		if(array1[i] === "9"){
-// 			array1[i] = arrayOfNumerals[x]+ arrayOfNumerals[x+2];
-// 		}
-// 		x=x+2;
-
-// 	}
-// 	var array3 = array1.reverse();
-// 	var result = array3.join("");
-// 	return result;
-// }
 
 $(function(event){
 	$("#submit").click(function(event){
